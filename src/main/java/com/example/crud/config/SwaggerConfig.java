882939dad43744315312527b8b40726325ce8156
kenomun <eugenio.mun.f@gmail.com>
2023-10-17 18:28:23 -0300

    crud con springboot, maven , postgres

---
+++ src/main/java/com/example/crud/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.crud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.crud.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                ;
+    }
+}
--- src/main/java/com/example/crud/controller/ProductController.java
+++ src/main/java/com/example/crud/controller/ProductController.java
@@ -2,12 +2,15 @@ package com.example.crud.controller;
 
 import com.example.crud.Service.ProductServiceImpl;
 import com.example.crud.models.Product;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
+
 
 @RestController
 @RequestMapping(path = "api/V1/products")
@@ -20,23 +23,27 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @ApiOperation(value = "Obtener todos los productos")
     @GetMapping
     public List<Product> getProducts(){
         return this.productService.getProducts();
     }
 
+    @ApiOperation(value = "Agregar un nueveo producto")
     @PostMapping
     public ResponseEntity<Object> registrarProduct(@RequestBody Product product){
         return this.productService.newProduct(product);
 
     }
 
+    @ApiOperation(value = "Editar un producto")
     @PutMapping
     public ResponseEntity<Object> updateProduct(@RequestBody Product product){
         return this.productService.newProduct(product);
 
     }
 
+    @ApiOperation(value = "Eliminar un producto")
     @DeleteMapping(path = "{productId}")
     public ResponseEntity<Object> DeleteProduct(@PathVariable("productId") Long id){
         return this.productService.deleteProduct(id);
