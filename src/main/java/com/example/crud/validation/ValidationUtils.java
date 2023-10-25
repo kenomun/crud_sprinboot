f33e04fcad15c88f79b2e5c6a351c7125f2ac687
kenomun <eugenio.mun.f@gmail.com>
2023-10-25 17:37:48 +0200

    Validación de campos

---
+++ src/main/java/com/example/crud/validation/ValidationUtils.java
@@ -0,0 +1,34 @@
+package com.example.crud.validation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ValidationUtils {
+
+    public ResponseEntity<Object> handleValidationErrors(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errors = new ArrayList<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errors.add(error.getDefaultMessage());
+            }
+
+            for (ObjectError error : bindingResult.getGlobalErrors()) {
+                errors.add(error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        }
+
+        return null; // No hay errores de validación
+    }
+}
+
